6880bab925010ad6264803c7babb2d6517e18a05
hppod <hppodgornik@gmail.com>
2020-12-12 15:16:21 -0300

    Implementando servidor rest

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+const express = require('express')
+const cors = require('cors')
 const db = require('./src/config/db')
+const app = express()
+const PORT = process.env.APP_PORT || 3000
 
-console.log('Test')
+app.use(cors())
+app.use(express.text())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json({ type: 'application/json' }))
+
+app.get('/', (req, res) => res.send('API listening'))
+app.use('*', (req, res) => res.send('API not found'))
+app.listen(PORT, (_) => console.log(`API listening on PORT ${PORT}`))
+
+module.exports = app
